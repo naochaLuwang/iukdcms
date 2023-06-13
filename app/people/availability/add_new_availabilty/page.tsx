7e9164539c47b33606baf1e61e05baa5dcb40049
Somojit Arambam <somoarambam123@gmail.com>
2023-06-13 13:03:56 +0530

    First Commit

--- app/people/availability/add_new_availabilty/page.tsx
+++ app/people/availability/add_new_availabilty/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import AvailabiltyForm from "../../../components/AvailabilityForm";
 
 const AddAvailability = () => {
   const handleSubmit = (timeSlots: any) => {
@@ -10,7 +9,6 @@ const AddAvailability = () => {
   return (
     <div className="container p-4 mx-auto">
       <h1 className="mb-4 text-2xl font-bold">Doctor Availability Form</h1>
-      <AvailabiltyForm onSubmit={handleSubmit} />
     </div>
   );
 };

5a7510d58b3a5b4168bd290a6153584bbcde0b7a
Somojit Arambam <somoarambam123@gmail.com>
2023-06-30 15:36:35 +0530

    First Commit

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -54,7 +54,7 @@ export const authOptions: AuthOptions = {
       return session;
     },
   },
-  secret: process.env.NEXT_AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);

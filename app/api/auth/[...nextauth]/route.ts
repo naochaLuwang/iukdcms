c8262b53e32ab1002e114a94bdf5c0b3e17cdd1e
Somojit Arambam <somoarambam123@gmail.com>
2023-08-04 21:55:22 +0530

    Mobile responsive added

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -54,7 +54,7 @@ export const authOptions: AuthOptions = {
       return session;
     },
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXT_AUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
